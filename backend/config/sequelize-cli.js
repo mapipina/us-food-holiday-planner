8385330b8d08fb4ed5bbc46045c64edb0ad9ce12
Mapi Piña <mpina09@gmail.com>
2025-11-06 15:49:27 -0500

    small tweaks. running into old issues. should be easy fix

--- backend/config/sequelize-cli.js
+++ backend/config/sequelize-cli.js
@@ -3,13 +3,8 @@ require('dotenv').config({ path: '../.env' });
 module.exports = {
 
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable: 'DATABASE_URL', 
     dialect: 'postgres', 
-    
     migrationsPath: './src/db/migrations',
     seedersPath: './src/db/seeders',
   },
