9e5192e9eef074fda2eb03de1174db9d4abc40e4
Mapi Piña <mpina09@gmail.com>
2025-11-13 11:52:00 -0500

    updated types

--- frontend/src/types/Holiday.ts
+++ frontend/src/types/Holiday.ts
@@ -1,6 +1,37 @@
-export interface Holiday {
+export interface HolidayData {
     id: number;
     title: string;
     description: string;
-    date: Date; // need to change this to luxon
+    main_meal: string;
+    date_mm_dd: string;
 }
+
+export interface RecipeDatum {
+    idMeal: string;
+    strMeal: string;
+    strInstructions: string;
+    strMealThumb: string;
+}
+
+export interface ApiListResponse {
+    status: APIStatusResponse;
+    results: number;
+    data: {
+        holidays: HolidayData[];
+    };
+}
+
+export interface ApiItemResponse {
+    status: APIStatusResponse;
+    data: {
+        holiday: HolidayData;
+    };
+}
+
+export interface ApiRecipeResponse {
+    status:  APIStatusResponse;
+    holidayTitle: string;
+    data: RecipeDatum[];
+}
+
+type APIStatusResponse = 'success' | 'fail' | 'error';
