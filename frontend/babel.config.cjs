6b4cc983718f9e6ee1ed18c78a54947b6997ca1b
Mapi Piña <mpina09@gmail.com>
2025-11-13 14:03:41 -0500

    cleanup

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,8 +1,7 @@
-// /frontend/babel.config.js
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }], // Target current Node version for tests
+    ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
-    ['@babel/preset-react', { runtime: 'automatic' }] // Required for React JSX transformation
+    ['@babel/preset-react', { runtime: 'automatic' }]
   ],
 };
